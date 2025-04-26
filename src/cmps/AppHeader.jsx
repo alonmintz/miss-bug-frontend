42b33c7fe83a692bfe6f7f27fbbe5379be48faed
alonmintz <92505398+alonmintz@users.noreply.github.com>
2025-04-26 20:05:41 +0300

    added basic login signup + store wip

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,23 +1,74 @@
-
-import { useEffect } from 'react'
-import { UserMsg } from './UserMsg'
-import { NavLink } from 'react-router-dom'
+import { useEffect } from "react";
+import { UserMsg } from "./UserMsg";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { LoginSignup } from "./LoginSignup.jsx";
+import { userService } from "../services/user.service.js";
+import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js";
 
 export function AppHeader() {
-    useEffect(() => {
-        // component did mount when dependancy array is empty
-    }, [])
+  // Will be in the store in the future~~
+  const [loggedinUser, setLoggedinUser] = useState(
+    userService.getLoggedinUser()
+  );
 
-    return (
-        <header className='app-header container'>
-            <div className='header-container'>
-                <h1>Bugs are Forever</h1>
-                <nav className='app-nav'>
-                    <NavLink to="/">Home</NavLink> |<NavLink to="/bug">Bugs</NavLink> |
-                    <NavLink to="/about">About</NavLink>
-                </nav>
+  async function onLogin(credentials) {
+    console.log(credentials);
+    try {
+      const user = await userService.login(credentials);
+      setLoggedinUser(user);
+    } catch (err) {
+      console.log("Cannot login :", err);
+      showErrorMsg(`Cannot login`);
+    }
+  }
+
+  async function onSignup(credentials) {
+    console.log(credentials);
+    try {
+      const user = await userService.signup(credentials);
+      setLoggedinUser(user);
+      showSuccessMsg(`Welcome ${user.fullname}`);
+    } catch (err) {
+      console.log("Cannot signup :", err);
+      showErrorMsg(`Cannot signup`);
+    }
+  }
+
+  async function onLogout() {
+    console.log("logout");
+    try {
+      await userService.logout();
+      setLoggedinUser(null);
+    } catch (err) {
+      console.log("can not logout");
+    }
+  }
+
+  return (
+    <header className="app-header container">
+      <section className="header-container">
+        <h1>Bugs are Forever</h1>
+
+        <section className="login-signup-container">
+          {!loggedinUser && (
+            <LoginSignup onLogin={onLogin} onSignup={onSignup} />
+          )}
+
+          {loggedinUser && (
+            <div className="user-preview">
+              <h3>Hello {loggedinUser.fullname}</h3>
+              <button onClick={onLogout}>Logout</button>
             </div>
-            <UserMsg />
-        </header>
-    )
+          )}
+        </section>
+
+        <nav className="app-nav">
+          <NavLink to="/">Home</NavLink> |<NavLink to="/bug">Bugs</NavLink> |
+          <NavLink to="/about">About</NavLink>
+        </nav>
+      </section>
+      <UserMsg />
+    </header>
+  );
 }
--- src/services/bug.service.js
+++ src/services/bug.service.js
@@ -1,30 +1,61 @@
+import Axios from "axios";
+import { storageService } from "./async-storage.service.js";
+import { utilService } from "./util.service.js";
 
-import { storageService } from './async-storage.service.js'
-import { utilService } from './util.service.js'
+var axios = Axios.create({
+  withCredentials: true,
+});
 
-const STORAGE_KEY = 'bugDB'
+const STORAGE_KEY = "bugDB";
+const BASE_URL =
+  process.env.NODE_ENV !== "development"
+    ? "/api/bug/"
+    : "//localhost:3030/api/bug/";
 
 export const bugService = {
-    query,
-    getById,
-    save,
-    remove,
+  query,
+  getById,
+  save,
+  remove,
+};
+
+async function query(filterBy = {}) {
+  try {
+    const { data: bugs } = await axios.get(BASE_URL, { params: filterBy });
+    return bugs;
+  } catch (err) {
+    console.log({ err });
+    throw err;
+  }
 }
 
+async function getById(bugId) {
+  try {
+    const { data: bug } = await axios.get(`${BASE_URL}${bugId}`);
+    return bug;
+  } catch (err) {
+    console.log({ err });
+    throw err;
+  }
+}
 
-function query() {
-    return storageService.query(STORAGE_KEY)
+async function remove(bugId) {
+  try {
+    await axios.delete(`${BASE_URL}${bugId}`);
+  } catch (err) {
+    console.log({ err });
+    throw err;
+  }
 }
-function getById(bugId) {
-    return storageService.get(STORAGE_KEY, bugId)
-}
-function remove(bugId) {
-    return storageService.remove(STORAGE_KEY, bugId)
-}
-function save(bug) {
-    if (bug._id) {
-        return storageService.put(STORAGE_KEY, bug)
-    } else {
-        return storageService.post(STORAGE_KEY, bug)
-    }
+
+async function save(bug) {
+  try {
+    const httpMethod = bug._id ? "put" : "post";
+    const url = bug._id ? BASE_URL + bug._id : BASE_URL;
+    const { data: savedBug } = await axios[httpMethod](url, bug);
+    return savedBug;
+  } catch (err) {
+    console.log({ err });
+    throw err;
+  }
 }
